17ddfc278d96b87a3c36a02194ae414b054328df
Coco <lzllzllhl@163.com>
2017-08-01 20:05:54 +0800

    Initial commit

---
+++ app/src/main/java/com/example/coco/sliderdemo/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.coco.sliderdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.daimajia.slider.library.Animations.DescriptionAnimation;
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
+
+import java.util.HashMap;
+
+import static com.example.coco.sliderdemo.R.id.slider;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SliderLayout mDemoSlider;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mDemoSlider = (SliderLayout) findViewById(slider);
+
+        HashMap<String, String> url_maps = new HashMap<String, String>();
+        url_maps.put("Hannibal", "http://static2.hypable.com/wp-content/uploads/2013/12/hannibal-season-2-release-date.jpg");
+        url_maps.put("Big Bang Theory", "http://tvfiles.alphacoders.com/100/hdclearart-10.png");
+        url_maps.put("House of Cards", "http://cdn3.nflximg.net/images/3093/2043093.jpg");
+        url_maps.put("Game of Thrones", "http://images.boomsbeat.com/data/images/full/19640/game-of-thrones-season-4-jpg.jpg");
+
+        for(String name : url_maps.keySet()) {
+            TextSliderView textSliderView = new TextSliderView(this);
+            textSliderView
+//                    //添加描述
+//                    .description(name)
+                    .image(url_maps.get(name))//添加网络图片的网址的集合
+                    .setScaleType(BaseSliderView.ScaleType.Fit);//图片填充类型
+            //传值
+//            textSliderView.bundle(new Bundle());
+//            textSliderView.getBundle()
+//                    .putString("extra", name);
+            mDemoSlider.addSlider(textSliderView);
+
+            //修改轮播动画
+            mDemoSlider.setPresetTransformer(SliderLayout.Transformer.ZoomIn);
+            mDemoSlider.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
+            mDemoSlider.setCustomAnimation(new DescriptionAnimation());
+            mDemoSlider.setDuration(4000);
+        }
+    }
+
+}
